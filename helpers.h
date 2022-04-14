dbfc103392685b95048e21909344a129fb651254
Jakub A. Štigler <jakubstig@gmail.com>
2022-04-14 14:38:07 +0200

    Added printArrayI for printing int arrays

--- helpers.h
+++ helpers.h
@@ -7,8 +7,10 @@
 void swap(float* f1, float* f2);
 // returns the index of the first occurence of the given character, -1 if not found
 int indexOf(char* str, const char c);
-// prints the given array into stdout
+// prints the given array of floats into stdout
 void printArray(float *arr, const int length, const char *sep, const char *append);
+// prints the given array of ints into stdout
+void printArrayI(int* arr, const int length, const char* sep, const char* append);
 
 void swap(float *f1, float *f2)
 {
@@ -43,6 +45,23 @@ void printArray(float* arr, const int length, const char* sep, const char* appen
         printf("%f%s", arr[i], sep);
 
     printf("%f%s", arr[length - 1], append);
+}
+
+void printArrayI(int* arr, const int length, const char* sep, const char* append)
+{
+    if (length <= 0 || !arr)
+        return;
+
+    if (!sep)
+        sep = " ";
+    
+    if (!append)
+        append = "\n";
+
+    for (int i = 0; i < length - 1; i++)
+        printf("%d%s", arr[i], sep);
+
+    printf("%d%s", arr[length - 1], append);
 }
 
 #endif // HELPERS_H_INCLUDED
